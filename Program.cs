eedf5e929270c1cc290abd080104bb01cde6a907
mokbulh22 <132774308+mokbulh22@users.noreply.github.com>
2023-05-06 12:56:09 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -97,9 +97,6 @@ namespace ifElsePracrice
             {
                 Console.WriteLine (" Hossan ");          // True
             }
-
-
-
 
 
         }
